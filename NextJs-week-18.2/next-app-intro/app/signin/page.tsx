da953f8767378f17a2f2b9fc30cf5febc3a26d7f
Tausif's Macbook <mohammed@Tausifss-MacBook-Air.local>
2025-01-03 17:03:05 +0530

    Format Init

--- NextJs-week-18.2/next-app-intro/app/signin/page.tsx
+++ NextJs-week-18.2/next-app-intro/app/signin/page.tsx
@@ -1,35 +1,28 @@
 "use client"
 import Link from "next/link";
 export default function signin() {
-    const handleSignin = (e: any) => {
-        e.preventDefault();
-        alert("Signed in")
-        console.log("Signed in")
-
-    }
-
     return (
         <div className="h-screen w-full flex flex-col justify-center items-center ">
-           <div className="flex justify-center">
-        <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
-                <div>
-                    <div className="px-20 py-2 flex">
-                        <div className="text-3xl font-extrabold text-black">
-                            Sign in
+            <div className="flex justify-center">
+                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                    <div>
+                        <div className="px-20 py-2 flex">
+                            <div className="text-3xl font-extrabold text-black">
+                                Sign in
+                            </div>
+                        </div>
+                        <div className="pt-2 flex flex-col justify-center items-center gap-2" >
+                            <LabelledInput type='text' placeholder="user@gmail.com" label="Username" />
+                            <LabelledInput label="Password" placeholder="password" />
+                            <Link href={'/blogs'}> <button
+                                className="border w-28 bg-blue-700 border-none py-2  text-white rounded-md flex justify-center"
+                                type="submit"
+                            > Sign in
+                            </button> </Link>
                         </div>
                     </div>
-                    <div className="pt-2 flex flex-col justify-center items-center gap-2" >
-                    <LabelledInput type='text' placeholder="user@gmail.com" label="Username" />
-                    <LabelledInput label="Password" placeholder="password" />
-                   <Link href={'/blogs'}> <button
-                        className="border w-28 bg-blue-700 border-none py-2  text-white rounded-md flex justify-center"
-                        type="submit"
-                    > Sign in
-                    </button> </Link>
-                    </div>
+                </div>
             </div>
-            </div>
-        </div>
         </div>
     )
 }
--- NextJs-week-18.2/next-app-intro/app/signup/page.tsx
+++ NextJs-week-18.2/next-app-intro/app/signup/page.tsx
@@ -3,49 +3,47 @@ import Link from "next/link"
 import { useState } from "react"
 
 
-export default function signup(){
-    const [email, setEmail]= useState('')
-    const [password, setPassword]= useState('')
+export default function signup() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    function handleSignup(e: any){
+    function handleSignup(e: any) {
         e.preventDefault();
         alert("signed up with email :" + email)
-        console.log("Signed up with email :", email , "password : " , password)
+        console.log("Signed up with email :", email, "password : ", password)
     }
-    return(
+    return (
         <div className="h-screen w-full flex flex-col justify-center items-center ">
-        <div className="border px-12 py-16 rounded bg-white ">
-        <h1 className="text-3xl text-black font-semibold flex justify-center ">Sign Up</h1>
-        
-        <form onSubmit={handleSignup}
-            className="flex flex-col p-2 gap-3 justify-center items-center ">
-            <LabelledInput label="Firstname" placeholder='John' type='text'/>
-            <LabelledInput label="Username" placeholder='alexjohn@gmail.com' type='text' onChange={(e: any)=> setEmail(e.target.value)}/>
-            <LabelledInput label="Password" placeholder='password' type='password'/>
-            <Link href={'/signin'}> <button 
-            className="border w-28 flex justify-center bg-blue-700 text-white py-2 rounded-md border-none"
-            type="submit"
-            > Sign up
-            </button> </Link>
-            
-        </form>
+            <div className="border px-12 py-16 rounded bg-white ">
+                <h1 className="text-3xl text-black font-semibold flex justify-center ">Sign Up</h1>
+                <form onSubmit={handleSignup}
+                    className="flex flex-col p-2 gap-3 justify-center items-center ">
+                    <LabelledInput label="Firstname" placeholder='John' type='text' />
+                    <LabelledInput label="Username" placeholder='alexjohn@gmail.com' type='text' onChange={(e: any) => setEmail(e.target.value)} />
+                    <LabelledInput label="Password" placeholder='password' type='password' />
+                    <Link href={'/signin'}> <button
+                        className="border w-28 flex justify-center bg-blue-700 text-white py-2 rounded-md border-none"
+                        type="submit"
+                    > Sign up
+                    </button> </Link>
+                </form>
             </div>
-    </div>
+        </div>
     )
 }
-interface  InputProps{
+interface InputProps {
     label: string,
-    placeholder : string,
-    type? : string,
-    onChange? : any
+    placeholder: string,
+    type?: string,
+    onChange?: any
 }
 
-function LabelledInput({ label, placeholder, type , onChange}: InputProps){
+function LabelledInput({ label, placeholder, type, onChange }: InputProps) {
     return <div>
         <label className="block mb-2 text-sm text-black font-semibold pt-2 ">{label}</label>
         <input type={type || 'text'} placeholder={placeholder} required
-        className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full px-10 p-2.5"  
-        onChange = {onChange}
+            className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full px-10 p-2.5"
+            onChange={onChange}
         />
     </div>
 }
