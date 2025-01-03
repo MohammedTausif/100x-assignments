0440c6be4aacf7d72922fbdd5ad0f716055131ca
Tausif's Macbook <mohammed@Tausifss-MacBook-Air.local>
2025-01-03 16:58:39 +0530

    sample blogs using dummy json  api

---
+++ NextJs-week-18.2/next-app-intro/app/blogs/page.tsx
@@ -0,0 +1,37 @@
+
+import axios from "axios"
+
+
+export default async function blogs(){
+    const blogs = await getBlogs()
+    return <div className="h-screen w-full bg-white">
+        <div className="h-40 w-full flex flex-col  justify-center items-center">
+        <h1 className="text-2xl font-bold text-indigo-700">HELLO ! </h1>
+        
+        <span className="text-lg font-semibold"> Welcome to Tasks Blogs</span> 
+        </div>
+        <div className="flex min-h-screen w-[100%] flex-wrap justify-center items-center gap-4 bg-white">
+        {
+        blogs.map((blog: ITodo, id: number)=> <TodoCard title={blog.title} completed={blog.completed} key={id}/>)
+        } 
+        </div>
+        
+    </div>
+}
+async function getBlogs(){
+  const response = await axios.get("https://jsonplaceholder.typicode.com/todos/")
+  return response.data
+}
+
+interface ITodo {
+    title: string,
+    completed : boolean
+}
+function TodoCard ({title, completed}: ITodo){
+    return <div className="bg-gray-100 w-52  h-40 rounded flex flex-col  items-center">
+        <div className="flex flex-col  p-2 text-black gap-3 justify-center items-center">
+            <h1> <span className="text-lg font-semibold">Title:</span>  {title}</h1>
+            <h2><span className="text-lg font-semibold">Completed:</span> {completed? "Done! ✅" : "Not Done ❌"}</h2>
+        </div>
+    </div>
+}
--- NextJs-week-18.2/next-app-intro/app/signup/page.tsx
+++ NextJs-week-18.2/next-app-intro/app/signup/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link"
 import { useState } from "react"
 
 
@@ -8,40 +9,43 @@ export default function signup(){
 
     function handleSignup(e: any){
         e.preventDefault();
-        alert("signed up with email :" + email +"  " + "password : "+password )
+        alert("signed up with email :" + email)
         console.log("Signed up with email :", email , "password : " , password)
     }
     return(
         <div className="h-screen w-full flex flex-col justify-center items-center ">
-        <div className="border px-12 py-16 rounded ">
-        <h1 className="text-2xl font-semibold flex justify-center ">Sign Up</h1>
+        <div className="border px-12 py-16 rounded bg-white ">
+        <h1 className="text-3xl text-black font-semibold flex justify-center ">Sign Up</h1>
+        
         <form onSubmit={handleSignup}
-            className="flex flex-col p-2 gap-3 justify-center items-center">
-            <input
-                className="px-6 py-2 rounded text-black"
-                type="email"
-                placeholder="Enter email"
-                name="username"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-            />
-            <input
-                className="px-6 py-2 rounded "
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Enter password"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-            />
-            <button 
-            className="border w-28 flex justify-center bg-blue-700 py-2 rounded-md border-none"
+            className="flex flex-col p-2 gap-3 justify-center items-center ">
+            <LabelledInput label="Firstname" placeholder='John' type='text'/>
+            <LabelledInput label="Username" placeholder='alexjohn@gmail.com' type='text' onChange={(e: any)=> setEmail(e.target.value)}/>
+            <LabelledInput label="Password" placeholder='password' type='password'/>
+            <Link href={'/signin'}> <button 
+            className="border w-28 flex justify-center bg-blue-700 text-white py-2 rounded-md border-none"
             type="submit"
             > Sign up
-            </button>
+            </button> </Link>
+            
         </form>
             </div>
     </div>
     )
+}
+interface  InputProps{
+    label: string,
+    placeholder : string,
+    type? : string,
+    onChange? : any
+}
+
+function LabelledInput({ label, placeholder, type , onChange}: InputProps){
+    return <div>
+        <label className="block mb-2 text-sm text-black font-semibold pt-2 ">{label}</label>
+        <input type={type || 'text'} placeholder={placeholder} required
+        className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full px-10 p-2.5"  
+        onChange = {onChange}
+        />
+    </div>
 }
